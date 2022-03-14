7efeda8ecfe1de53eec8a761786bdb6ac077dbf1
beatrizfigueiroa <beatrizramosfig@hotmail.com>
2022-03-14 10:43:41 -0300

    Creating interface class

---
+++ unikut/Interface.java
@@ -0,0 +1,8 @@
+package unikut;
+
+public class Interface {
+    public static void main(String[] args) {
+        System.out.println("UNIKUT");
+        System.out.println("Bem vindo :)");
+    }
+}
